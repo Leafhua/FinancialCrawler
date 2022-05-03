5a90e8ecb162717e49a12924e5960cc5395ce7c0
zhuhua <1007664276@qq.com>
2022-05-04 01:25:29 +0800

    数据分析模块，完成股票数据清洗和绘制k线图

---
+++ data_analysis/data_ana/financial_ana_kline.py
@@ -0,0 +1,244 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/5/3 16:40
+# @Author  : zhuhua
+# @File    : financial_ana_kline.py
+# @Software: PyCharm
+
+from typing import List, Union
+
+import pandas as pd
+from pyecharts import options as opts
+from pyecharts.charts import Kline, Line, Bar, Grid
+
+df = pd.read_csv('..\\resources\\stockBoardEm.csv')
+df["ISO时间"] = pd.to_datetime(df["ISO时间"]).dt.date
+
+
+def select_datasource_by_code(code):
+    return df[(df["股票代码"] == code)].drop_duplicates(["ISO时间"], keep='last')
+
+
+def select_datasource_by_name(name):
+    return df[(df["股票名称"] == name)].drop_duplicates(["ISO时间"], keep='last')
+
+
+# cleaned_data = select_datasource_by_name("福瑞股份")
+# cleaned_data = cleaned_data[["今开", "昨收", "最低", "最高", "成交量", "ISO时间"]]
+
+# a = select_datasource_by_code(300464)
+# a = a[["ISO时间", "今开", "昨收", "最低", "最高", "成交量", "股票名称"]]
+# a["ISO时间"] = a["ISO时间"].apply(lambda x: x.strftime('%Y-%m-%d'))
+# b = a.values.tolist()
+# print(b[0][-1])
+
+# x_data = cleaned_data["ISO时间"].apply(lambda x: x.strftime('%Y-%m-%d')).tolist()
+# y_data = cleaned_data[["今开", "昨收", "最低", "最高"]].values.tolist()
+
+
+def get_data(code):
+    cleaned_data = select_datasource_by_code(code)[["ISO时间", "今开", "昨收", "最低", "最高", "成交量", "股票名称"]]
+    cleaned_data["ISO时间"] = cleaned_data["ISO时间"].apply(lambda x: x.strftime('%Y-%m-%d'))
+
+    # 解析数据
+    return split_data(data=cleaned_data.values.tolist())
+
+
+def split_data(data):
+    category_data = []
+    values = []
+    volumes = []
+
+    for i, tick in enumerate(data):
+        category_data.append(tick[0])
+        values.append(tick)
+        volumes.append([i, tick[4], 1 if tick[1] > tick[2] else -1])
+    return {"categoryData": category_data, "values": values, "volumes": volumes, "name": data[0][-1]}
+
+
+def calculate_ma(day_count: int, data):
+    result: List[Union[float, str]] = []
+    for i in range(len(data["values"])):
+        if i < day_count:
+            result.append("-")
+            continue
+        sum_total = 0.0
+        for j in range(day_count):
+            sum_total += float(data["values"][i - j][1])
+        result.append(abs(float("%.3f" % (sum_total / day_count))))
+    return result
+
+
+def draw_charts():
+    kline_data = [data[1:-1] for data in chart_data["values"]]
+    kline = (
+        Kline()
+        .add_xaxis(xaxis_data=chart_data["categoryData"])
+        .add_yaxis(
+            series_name=chart_data["name"],
+            y_axis=kline_data,
+            itemstyle_opts=opts.ItemStyleOpts(color="#ec0000", color0="#00da3c"),
+        )
+        .set_global_opts(
+            legend_opts=opts.LegendOpts(
+                is_show=False, pos_bottom=10, pos_left="center"
+            ),
+            datazoom_opts=[
+                opts.DataZoomOpts(
+                    is_show=False,
+                    type_="inside",
+                    xaxis_index=[0, 1],
+                    range_start=98,
+                    range_end=100,
+                ),
+                opts.DataZoomOpts(
+                    is_show=True,
+                    xaxis_index=[0, 1],
+                    type_="slider",
+                    pos_top="85%",
+                    range_start=98,
+                    range_end=100,
+                ),
+            ],
+            yaxis_opts=opts.AxisOpts(
+                is_scale=True,
+                splitarea_opts=opts.SplitAreaOpts(
+                    is_show=True, areastyle_opts=opts.AreaStyleOpts(opacity=1)
+                ),
+            ),
+            tooltip_opts=opts.TooltipOpts(
+                trigger="axis",
+                axis_pointer_type="cross",
+                background_color="rgba(245, 245, 245, 0.8)",
+                border_width=1,
+                border_color="#ccc",
+                textstyle_opts=opts.TextStyleOpts(color="#000"),
+            ),
+            visualmap_opts=opts.VisualMapOpts(
+                is_show=False,
+                dimension=2,
+                series_index=5,
+                is_piecewise=True,
+                pieces=[
+                    {"value": 1, "color": "#00da3c"},
+                    {"value": -1, "color": "#ec0000"},
+                ],
+            ),
+            axispointer_opts=opts.AxisPointerOpts(
+                is_show=True,
+                link=[{"xAxisIndex": "all"}],
+                label=opts.LabelOpts(background_color="#777"),
+            ),
+            brush_opts=opts.BrushOpts(
+                x_axis_index="all",
+                brush_link="all",
+                out_of_brush={"colorAlpha": 0.1},
+                brush_type="lineX",
+            ),
+        )
+    )
+
+    line = (
+        Line()
+        .add_xaxis(xaxis_data=chart_data["categoryData"])
+        .add_yaxis(
+            series_name="MA5",
+            y_axis=calculate_ma(day_count=5, data=chart_data),
+            is_smooth=True,
+            is_hover_animation=False,
+            linestyle_opts=opts.LineStyleOpts(width=3, opacity=0.5),
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+        .add_yaxis(
+            series_name="MA10",
+            y_axis=calculate_ma(day_count=10, data=chart_data),
+            is_smooth=True,
+            is_hover_animation=False,
+            linestyle_opts=opts.LineStyleOpts(width=3, opacity=0.5),
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+        .add_yaxis(
+            series_name="MA20",
+            y_axis=calculate_ma(day_count=20, data=chart_data),
+            is_smooth=True,
+            is_hover_animation=False,
+            linestyle_opts=opts.LineStyleOpts(width=3, opacity=0.5),
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+        .add_yaxis(
+            series_name="MA30",
+            y_axis=calculate_ma(day_count=30, data=chart_data),
+            is_smooth=True,
+            is_hover_animation=False,
+            linestyle_opts=opts.LineStyleOpts(width=3, opacity=0.5),
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+        .set_global_opts(xaxis_opts=opts.AxisOpts(type_="category"))
+    )
+
+    bar = (
+        Bar()
+        .add_xaxis(xaxis_data=chart_data["categoryData"])
+        .add_yaxis(
+            series_name="Volume",
+            y_axis=chart_data["volumes"],
+            xaxis_index=1,
+            yaxis_index=1,
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+        .set_global_opts(
+            xaxis_opts=opts.AxisOpts(
+                type_="category",
+                is_scale=True,
+                grid_index=1,
+                boundary_gap=False,
+                axisline_opts=opts.AxisLineOpts(is_on_zero=False),
+                axistick_opts=opts.AxisTickOpts(is_show=False),
+                splitline_opts=opts.SplitLineOpts(is_show=False),
+                axislabel_opts=opts.LabelOpts(is_show=False),
+                split_number=20,
+                min_="dataMin",
+                max_="dataMax",
+            ),
+            yaxis_opts=opts.AxisOpts(
+                grid_index=1,
+                is_scale=True,
+                split_number=2,
+                axislabel_opts=opts.LabelOpts(is_show=False),
+                axisline_opts=opts.AxisLineOpts(is_show=False),
+                axistick_opts=opts.AxisTickOpts(is_show=False),
+                splitline_opts=opts.SplitLineOpts(is_show=False),
+            ),
+            legend_opts=opts.LegendOpts(is_show=False),
+        )
+    )
+
+    # Kline And Line
+    overlap_kline_line = kline.overlap(line)
+
+    # Grid Overlap + Bar
+    grid_chart = Grid(
+        init_opts=opts.InitOpts(
+            width="1000px",
+            height="800px",
+            animation_opts=opts.AnimationOpts(animation=False),
+        )
+    )
+    grid_chart.add(
+        overlap_kline_line,
+        grid_opts=opts.GridOpts(pos_left="10%", pos_right="8%", height="50%"),
+    )
+    grid_chart.add(
+        bar,
+        grid_opts=opts.GridOpts(
+            pos_left="10%", pos_right="8%", pos_top="63%", height="16%"
+        ),
+    )
+
+    grid_chart.render("..\\resources\\html\\kline_"+chart_data["name"]+".html")
+
+
+if __name__ == "__main__":
+    chart_data = get_data(300199)
+    draw_charts()
+
+
---
+++ data_analysis/data_clean/stock_data.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/5/3 17:01
+# @Author  : zhuhua
+# @File    : stock_data.py
+# @Software: PyCharm
+import pandas as pd
+import pymongo
+
+# DataBase Setting
+MONGODB_HOST = "127.0.0.1"
+MONGODB_PORT = 27017
+MONGODB_DBNAME = "FinancialCrawler"
+MONGODB_SHEETNAME = "stockBoardEm"
+
+# 连接数据库
+client = pymongo.MongoClient(host=MONGODB_HOST, port=MONGODB_PORT)
+stock_cur = client[MONGODB_DBNAME][MONGODB_SHEETNAME]
+
+# 数据清洗
+data = pd.DataFrame(list(stock_cur.find()))
+del data['_id']
+data.columns = [
+    "最新价",
+    "涨跌幅",
+    "涨跌额",
+    "成交量",
+    "成交额",
+    "振幅",
+    "换手率",
+    "市盈率-动态",
+    "量比",
+    "股票代码",
+    "股票名称",
+    "最高",
+    "最低",
+    "今开",
+    "昨收",
+    "市净率",
+    "ISO时间",
+]
+data["最新价"] = pd.to_numeric(data["最新价"], errors='coerce')
+data["涨跌幅"] = pd.to_numeric(data["涨跌幅"], errors='coerce')
+data["涨跌额"] = pd.to_numeric(data["涨跌额"], errors='coerce')
+data["成交量"] = pd.to_numeric(data["成交量"], errors='coerce')
+data["成交额"] = pd.to_numeric(data["成交额"], errors='coerce')
+data["振幅"] = pd.to_numeric(data["振幅"], errors='coerce')
+data["换手率"] = pd.to_numeric(data["换手率"], errors='coerce')
+data["市盈率-动态"] = pd.to_numeric(data["市盈率-动态"], errors='coerce')
+data["量比"] = pd.to_numeric(data["量比"], errors='coerce')
+data["股票代码"] = pd.to_numeric(data["股票代码"], errors='coerce')
+data["最高"] = pd.to_numeric(data["最高"], errors='coerce')
+data["最低"] = pd.to_numeric(data["最低"], errors='coerce')
+data["今开"] = pd.to_numeric(data["今开"], errors='coerce')
+data["昨收"] = pd.to_numeric(data["昨收"], errors='coerce')
+data["市净率"] = pd.to_numeric(data["市净率"], errors='coerce')
+data["ISO时间"] = pd.to_datetime(data["ISO时间"], errors='coerce')
+
+with open('..\\resources\\' + MONGODB_SHEETNAME + '.csv', 'w', encoding='utf-8-sig', newline='') as f:
+    data.to_csv(f, index=None)
+print(data.dtypes)
+
